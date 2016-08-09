06fccd8c53ea2c9d1b31ee466716ed1b0cad354d
DrazenRocket <drazen.rocket@hotmail.com>
2016-08-09 13:44:42 +0200

    Initial commit for created project.

---
+++ src/certgen/app/Application.java
@@ -0,0 +1,15 @@
+package certgen.app;
+
+/**
+ * Class with method which represent entry 
+ * point for application of this project.
+ * 
+ * @author Dražen
+ */
+public class Application {
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
